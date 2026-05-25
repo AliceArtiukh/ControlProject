4a5daddc84ff0dc0d43b8e2c8636cfc899ecdbcf
AliceArtiukh <alisa_artiukh1@knu.ua>
2026-05-25 21:57:00 +0300

    Cleanup: Remove unused variables from 11.1.2.py
    Removed initial configuration variables for Tetris game.

--- 11.1.2.py
+++ 11.1.2.py
@@ -1,15 +1,5 @@
 import turtle
 import time
-<<<<<<< HEAD
-rows=20
-cols=10
-screen_width=400
-screen_height=600
-pedding_x=150
-pedding_y=100
-cell_size=min((screen_width-pedding_x)/cols, (screen_height-pedding_y)/rows)
-print(cell_size)
-=======
 
 
 from Game import TetrisGame
