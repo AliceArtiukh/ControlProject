23494c2351639075fce98e7297323265e5a725ca
AliceArtiukh <alisa_artiukh1@knu.ua>
2026-05-25 19:37:35 +0300

    Remove colors and shapes definitions

--- 11.1.2.py
+++ 11.1.2.py
@@ -8,6 +8,4 @@ screen_height=600
 pedding_x=150
 pedding_y=100
 cell_size=min((screen_width-pedding_x)/cols, (screen_height-pedding_y)/rows)
-colors=['red','green','blue','yellow','magenta','cyan', 'orange']
-shapes=[(0,-1),(0,0),(0,1),(0,2)]
 print(cell_size)
