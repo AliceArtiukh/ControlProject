4505ed25080599baf0cf89547d0cbd0f1f8b7bb6
AliceArtiukh <alisa_artiukh1@knu.ua>
2026-05-25 21:37:45 +0300

    third

--- 11.1.2.py
+++ 11.1.2.py
@@ -1,10 +1,7 @@
 import turtle
 import time
-rows=20
-cols=10
-screen_width=400
-screen_height=600
-pedding_x=150
-pedding_y=100
-cell_size=min((screen_width-pedding_x)/cols, (screen_height-pedding_y)/rows)
-print(cell_size)
+
+
+from Game import TetrisGame
+if __name__ == '__main__':
+    game=TetrisGame()
