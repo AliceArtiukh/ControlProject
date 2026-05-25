1af2a4dc0ffbb69aeda81e0a796a23dd780ed243
AliceArtiukh <alisa_artiukh1@knu.ua>
2026-05-25 19:31:16 +0300

    Second

--- 11.1.2.py
+++ 11.1.2.py
@@ -1,4 +1,3 @@
-import random
 import turtle
 import time
 rows=20
@@ -8,6 +7,4 @@ screen_height=600
 pedding_x=150
 pedding_y=100
 cell_size=min((screen_width-pedding_x)/cols, (screen_height-pedding_y)/rows)
-colors=['red','green','blue','yellow','magenta','cyan', 'orange']
-shapes=[(0,-1),(0,0),(0,1),(0,2)]
 print(cell_size)
