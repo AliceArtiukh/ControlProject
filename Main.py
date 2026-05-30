680db0855e9a31d243d4e1c7c74ede6afce0944e
AliceArtiukh <alisa_artiukh1@knu.ua>
2026-05-30 19:58:24 +0300

    nineth
    # Conflicts:
    #	Main.py

--- Main.py
+++ Main.py
@@ -1,23 +1,9 @@
-import time
-from tetris_game import TetrisGame
+import turtle
+from tkinter import messagebox
 
 
-def main():
-  game = TetrisGame()
-  last_update_time = time.time()
-
-  while True:
-   current_time = time.time()
-
-   if current_time - last_update_time > 0.4:
-      game.update_logic()
-      last_update_time = current_time
-
-   game.render()
-   time.sleep(0.01)
-
-
-if __name__ == "__main__":
-    main()
-
+from Tetris import TetrisGame
+if __name__ == '__main__':
+    game=TetrisGame()
+    game.start_loop()
 
