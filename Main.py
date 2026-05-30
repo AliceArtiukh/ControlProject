82002f9ebd34310f0d411159cf27dd6f6d6cd44e
rozhnovmaxim <rozhnov.maxim@knu.ua>
2026-05-30 19:43:18 +0300

    прописав код для файлу Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,23 @@
-import turtle
-from tkinter import messagebox
+import time
+from tetris_game import TetrisGame
 
 
-from Tetris import TetrisGame
-if __name__ == '__main__':
-    game=TetrisGame()
-    game.start_loop()
+def main():
+  game = TetrisGame()
+  last_update_time = time.time()
+
+  while True:
+   current_time = time.time()
+
+   if current_time - last_update_time > 0.4:
+      game.update_logic()
+      last_update_time = current_time
+
+   game.render()
+   time.sleep(0.01)
+
+
+if __name__ == "__main__":
+    main()
+
 
